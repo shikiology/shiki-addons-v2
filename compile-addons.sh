ee83e22ae6399959e26436e08b12d3f290e6b03f
@andatoshiki <hello@toshiki.dev>
2024-03-12 20:10:32 -0700

    fix(ci): update compilation script for matching original v1 version and add ci trigger event with git push action

--- compile-addons.sh
+++ compile-addons.sh
@@ -3,14 +3,14 @@
 set -e
 
 TMP_PATH="/tmp"
-YQ_BIN="$(dirname $0)/yq"
-TOOLKIT_VER="7.1-amd64-ubuntu"
+YQ_BIN="`dirname $0`/yq"
+TOOLKIT_VER="7.2-amd64-ubuntu"
 
 ###############################################################################
 #
 # 1 - Path of key
 function hasConfigKey() {
-  [ "$(${YQ_BIN} eval '.'${1}' | has("'${2}'")' "${3}")" == "true" ] && return 0 || return 1
+  [ "`${YQ_BIN} eval '.'${1}' | has("'${2}'")' "${3}"`" == "true" ] && return 0 || return 1
 }
 
 ###############################################################################
@@ -19,7 +19,7 @@ function hasConfigKey() {
 # 2 - Path of yaml config file
 # Return Value
 function readConfigKey() {
-  RESULT=$(${YQ_BIN} eval '.'${1}' | explode(.)' "${2}")
+  RESULT=`${YQ_BIN} eval '.'${1}' | explode(.)' "${2}"`
   [ "${RESULT}" == "null" ] && echo "" || echo ${RESULT}
 }
 
@@ -44,14 +44,14 @@ function compile-addon() {
   OUT_PATH="${TMP_PATH}/${1}"
   rm -rf "${OUT_PATH}"
   mkdir -p "${OUT_PATH}"
-  VER=$(readConfigKey "version" "${MANIFEST}")
+  VER=`readConfigKey "version" "${MANIFEST}"`
   # Check manifest version
   if [ ${VER} -ne 1 ]; then
     echo -e "\033[1;44mWarning: version ${VER} of manifest not suported, ignoring it\033[0m"
     return 0
   fi
   # Check if has compile script
-  COMPILESCRIPT=$(readConfigKey "compile-script" "${MANIFEST}")
+  COMPILESCRIPT=`readConfigKey "compile-script" "${MANIFEST}"`
   if [ -n "${COMPILESCRIPT}" ]; then
     echo "Running compile script"
     pushd . >/dev/null
@@ -66,7 +66,7 @@ function compile-addon() {
     echo -e "\033[1;32m Processing 'all' section\033[0m"
     HAS_FILES=0
     # Get name of script to install, if defined. This script has low priority
-    INSTALL_SCRIPT="$(readConfigKey "all.install-script" "${MANIFEST}")"
+    INSTALL_SCRIPT="`readConfigKey "all.install-script" "${MANIFEST}"`"
     if [ -n "${INSTALL_SCRIPT}" ]; then
       if [ -f "${1}/${INSTALL_SCRIPT}" ]; then
         echo -e "\033[1;35m  Copying install script ${INSTALL_SCRIPT}\033[0m"
@@ -78,7 +78,7 @@ function compile-addon() {
       fi
     fi
     # Get folder name for copy
-    COPY_PATH="$(readConfigKey "all.copy" "${MANIFEST}")"
+    COPY_PATH="`readConfigKey "all.copy" "${MANIFEST}"`"
     # If folder exists, copy
     if [ -n "${COPY_PATH}" ]; then
       if [ -d "${1}/${COPY_PATH}" ]; then
@@ -102,7 +102,7 @@ function compile-addon() {
   # Now check files for individual models
   unset AVAL_FOR
   declare -a AVAL_FOR
-  for P in $(readConfigEntriesArray "available-for" "${MANIFEST}"); do
+  for P in `readConfigEntriesArray "available-for" "${MANIFEST}"`; do
     AVAL_FOR+=(${P})
   done
 
@@ -111,7 +111,7 @@ function compile-addon() {
     echo -e "\033[1;32m Processing '${P}' platform-kver section\033[0m"
     HAS_FILES=0
     # Get name of script to install, if defined. This script has high priority
-    INSTALL_SCRIPT="$(readConfigKey 'available-for."'${P}'".install-script' "${MANIFEST}")"
+    INSTALL_SCRIPT="`readConfigKey 'available-for."'${P}'".install-script' "${MANIFEST}"`"
     if [ -n "${INSTALL_SCRIPT}" ]; then
       if [ -f "${1}/${INSTALL_SCRIPT}" ]; then
         echo -e "\033[1;35m  Copying install script ${INSTALL_SCRIPT}\033[0m"
@@ -123,7 +123,7 @@ function compile-addon() {
       fi
     fi
     # Get folder name for copy
-    COPY_PATH="$(readConfigKey 'available-for."'${P}'".copy' "${MANIFEST}")"
+    COPY_PATH="`readConfigKey 'available-for."'${P}'".copy' "${MANIFEST}"`"
     # If folder exists, copy
     if [ -n "${COPY_PATH}" ]; then
       if [ -d "${1}/${COPY_PATH}" ]; then
@@ -135,18 +135,18 @@ function compile-addon() {
         echo -e "\033[1;33m  WARNING: folder '${1}/${COPY_PATH}' not found\033[0m"
       fi
     fi
-    HAS_MODULES="$(readConfigKey 'available-for."'${P}'".modules' "${MANIFEST}")"
+    HAS_MODULES="`readConfigKey 'available-for."'${P}'".modules' "${MANIFEST}"`"
     # Check if has modules for compile
     if [ "${HAS_MODULES}" = "true" ]; then
       echo "Compiling modules"
-      PLATFORM="$(echo ${P} | cut -d'-' -f1)"
-      KVER="$(echo ${P} | cut -d'-' -f2)"
+      PLATFORM="`echo ${P} | cut -d'-' -f1`"
+      KVER="`echo ${P} | cut -d'-' -f2`"
       # Compile using docker
       rm -rf "${TMP_PATH}/${1}-mods"
       mkdir -p "${TMP_PATH}/${1}-mods"
-      #      docker run --rm -t -v "${TMP_PATH}/${1}-mods":/output \
-      #        -v "${PWD}/${1}/src/${KVER}":/input fbelavenuto/syno-toolkit:${PLATFORM}-${TOOLKIT_VER} compile-module
-      docker run --rm -t --user $(id -u) -v "${TMP_PATH}/${1}-mods":/output \
+#      docker run --rm -t -v "${TMP_PATH}/${1}-mods":/output \
+#        -v "${PWD}/${1}/src/${KVER}":/input andatoshiki/syno-toolkit:${PLATFORM}-${TOOLKIT_VER} compile-module
+      docker run --rm -t --user `id -u` -v "${TMP_PATH}/${1}-mods":/output \
         -v "${PWD}/${1}/src/${KVER}":/input andatoshiki/shiki-compiler:${TOOLKIT_VER} compile-module ${PLATFORM}
       mkdir -p "${OUT_PATH}/${P}/root/modules"
       mv "${TMP_PATH}/${1}-mods/"*.ko "${OUT_PATH}/${P}/root/modules/"
@@ -173,7 +173,7 @@ if [ $# -ge 1 ]; then
   done
 else
   while read D; do
-    DRIVER=$(basename ${D})
+    DRIVER=`basename ${D}`
     [ "${DRIVER:0:1}" = "." ] && continue
     compile-addon ${DRIVER}
   done < <(find -maxdepth 1 -type d)
